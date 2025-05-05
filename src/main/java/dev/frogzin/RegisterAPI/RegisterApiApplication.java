802f37bdeb9572493f7895204c6f4b446a2d8e4f
frogzinkk <andre.pereira@ucsal.edu.br>
2025-05-04 22:18:24 -0300

    first commit

---
+++ src/main/java/dev/frogzin/RegisterAPI/RegisterApiApplication.java
@@ -0,0 +1,13 @@
+package dev.frogzin.RegisterAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterApiApplication.class, args);
+	}
+
+}
